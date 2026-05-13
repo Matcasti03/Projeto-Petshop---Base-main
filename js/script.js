41210b6e06fa846a39479ec6adaa0245116bcb67
Matcast-ox <matcastilho03@gmail.com>
2026-05-13 10:37:55 -0300

    s

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // scripts petshop
+
+document.getElementById("formContato").addEventListener("submit", function(e) {
+  e.preventDefault();
+    let nome= document.getElementById("nome").value.trim();
+    let email= document.getElementById("email").value.trim();   
+    let mensagem= document.getElementById("mensagem").value.trim();
+
+    if(nome === "" || email === "" || mensagem === "") {
+      alert("Por favor, preencha todos os campos!");
+      return;
+    }else{
+        alert("mensagem enviada com sucesso");
+
+    }
+
+    this.reset();
+});
+//******************************************************************//
+let botaoMenu = document.getElementById("menu-btn");
+let menu = document.getElementById("menu");
+botaoMenu.classList.toggle("click", () => {
+    menu.classList.toggle(Ativo);
+})
