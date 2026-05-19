6b121afe4d4facb517c4b385b54251ca70f14e3d
Matcast-ox <matcastilho03@gmail.com>
2026-05-19 09:29:15 -0300

    12

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-// scripts petshop
-
 document.getElementById("formContato").addEventListener("submit", function(e) {
   e.preventDefault();
-    let nome= document.getElementById("nome").value.trim();
-    let email= document.getElementById("email").value.trim();   
-    let mensagem= document.getElementById("mensagem").value.trim();
+  
+  let nome = document.getElementById("nome").value.trim();
+  let email = document.getElementById("email").value.trim();   
+  let mensagem = document.getElementById("mensagem").value.trim();
 
-    if(nome === "" || email === "" || mensagem === "") {
-      alert("Por favor, preencha todos os campos!");
-      return;
-    }else{
-        alert("mensagem enviada com sucesso");
+  if (nome === "" || email === "" || mensagem === "") {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  } else {
+    alert("Mensagem enviada com sucesso!");
+  }
 
-    }
-
-    this.reset();
+  this.reset();
 });
-//******************************************************************//
+
 let botaoMenu = document.getElementById("menu-btn");
 let menu = document.getElementById("menu");
-botaoMenu.classList.toggle("click", () => {
-    menu.classList.toggle(Ativo);
-})
 
+botaoMenu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+});
 
+const botaoTema = document.getElementById('theme-toggle');
+
+botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
